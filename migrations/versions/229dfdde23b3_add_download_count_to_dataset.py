c04d53a9b485453b6ade6ffa1e1ce16506cae07c
Javier Lozano Gómez <javloz9495@gmail.com>
2025-11-11 01:47:18 +0100

    feat(featuretask/WI-105-2-Logica-negocio): Incrementar download_count, proteger con transaccion para evitar incoherencias y log añadido para comproar el funcionamiento

---
+++ migrations/versions/229dfdde23b3_add_download_count_to_dataset.py
@@ -0,0 +1,27 @@
+"""add download count to dataset
+
+Revision ID: 229dfdde23b3
+Revises: 2bbe9c8ccd1d
+Create Date: 2025-11-11 01:45:00.000000
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "229dfdde23b3"
+down_revision = "2bbe9c8ccd1d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        "data_set",
+        sa.Column("download_count", sa.Integer(), nullable=False, server_default="0"),
+    )
+
+
+def downgrade():
+    op.drop_column("data_set", "download_count")
