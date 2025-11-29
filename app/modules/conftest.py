15f6dfb15643326a11be9d9045e8e6b2750755f7
Daniel Reyes Zajara <danreyzaj@alum.us.es>
2025-11-29 16:28:39 +0100

    feat(WI-89): implement encrypted recovery code lifecycle

--- app/modules/conftest.py
+++ app/modules/conftest.py
@@ -42,14 +42,15 @@ def test_client(test_app):
 
 
 @pytest.fixture(scope="function")
-def clean_database():
-    db.session.remove()
-    db.drop_all()
-    db.create_all()
-    yield
-    db.session.remove()
-    db.drop_all()
-    db.create_all()
+def clean_database(test_app):
+    with test_app.app_context():
+        db.session.remove()
+        db.drop_all()
+        db.create_all()
+        yield
+        db.session.remove()
+        db.drop_all()
+        db.create_all()
 
 
 def login(test_client, email, password):
